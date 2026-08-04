e87bdd1ea1e484fef1700edd2aeadb6a71b25218
ghostf049-collab <ghostf049@gmail.com>
2026-08-04 17:12:56 +0200

    Create messageStyle.js

---
+++ utils/messageStyle.js
@@ -0,0 +1,18 @@
+import fs from "fs"
+import stylizedChar from "./fancy.js"
+
+export default function stylizedCardMessage(text) {
+  return {
+    text: stylizedChar(text),
+    contextInfo: {
+      externalAdReply: {
+        title: "Dark bot",
+        body: "𝐍𝐨𝐜𝐭𝐢𝐬",
+        thumbnail: fs.readFileSync("./database/DigiX.jpg"),
+        sourceUrl: "https://whatsapp.com",
+        mediaType: 1,
+        renderLargerThumbnail: false
+      }
+    }
+  }
+}
